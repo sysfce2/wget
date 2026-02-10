7ae78ab856615f22a4dfd32bcd3a0c211c602670
Darshit Shah <darnir@gnu.org>
2026-02-10 17:58:52 +0100

    cfg.mk: Disable the codespell check
    The new sc_codespell check introduces a lot of false positives. Until we
    can clean it up, it is better to disable it and allow our CI pipelines
    to work.

--- cfg.mk
+++ cfg.mk
@@ -20,7 +20,8 @@ local-checks-to-skip = \
     sc_cast_of_argument_to_free \
     sc_prohibit_gnu_make_extensions \
 	\
-	sc_indent
+	sc_indent \
+	sc_codespell
 
 #SHELL=bash -x
 show-vc-list-except:
